b4060d570c0603dfe684643522006508c957b89c
Tasos Laskos <tasos.laskos@gmail.com>
2024-04-15 20:39:08 +0300

    Entry list: Maintain display status of expanded note rows

--- app/assets/javascripts/channels/scan_result_channel.js
+++ app/assets/javascripts/channels/scan_result_channel.js
@@ -11,9 +11,16 @@ $(document).on('turbo:load', function() {
 
   App.cable.subscriptions.create(channel, {
     received() {
+      var opened = [];
+      $('tr.entry-notes:visible').each( function () {
+        opened.push( $(this).attr('id') );
+      });
+
       $.ajax({
         url: window.location.pathname + window.location.search,
-        dataType: 'script'
+        dataType: 'script',
+      }).done(function () {
+        $(opened).each( function (){ $('#' + this).toggle() } )
       })
     }
   });
