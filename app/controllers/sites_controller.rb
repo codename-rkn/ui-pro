2ad8f2560dbbaba734d4f7b10e40c6262658aad5
Tasos Laskos <tasos.laskos@gmail.com>
2024-04-16 17:43:53 +0300

    Whitespace fix

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -100,7 +100,7 @@ class SitesController < ApplicationController
             sitemap:   @site.sitemap_entries,
             scans:     @scans.order( id: :desc ),
             revisions: @site.revisions.order( id: :desc ),
-            entries:    scan_results_entries
+            entries:   scan_results_entries
         )
     end
 
