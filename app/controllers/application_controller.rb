80c6431eabd674101e1435c6a0222ec7ca625720
zapotek <@3$%1pr0t0S3CW3b4P&8)(>
2025-12-03 15:42:14 +0200

    Fixed empty timezone bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,11 @@ class ApplicationController < ActionController::Base
 
     private
     def set_time_zone
+      if !Settings.timezone.blank?
         Time.use_zone( Settings.timezone ) { yield }
+      else
+        yield
+      end
     end
 
 end
